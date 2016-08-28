537a0e025063ba423cddf0de1f0b32aef3a5f888
Hilmi Hassan <mhilmi1117@gmail.com>
2016-08-28 21:17:07 +0800

    Form Post

--- app.js
+++ app.js
@@ -8,35 +8,26 @@ app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
+var bodyParser = require('body-parser');
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 // This responds with "Hello World" on the homepage
 app.get('/', (req, res) => {
     console.log("Got a GET request for the homepage");
-    res.render('home');
+    res.render('home', { name: 'YOUR_NAME' });
 })
 
-// This responds a POST request for the homepage
-app.post('/', (req, res) => {
-    console.log("Got a POST request for the homepage");
-    res.send('Hello POST');
+app.post('/process_post', urlencodedParser, function (req, res) {
+ // Prepare output in JSON format
+    var response = {
+        first_name:req.body.first_name,
+        last_name:req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 })
 
-// This responds a DELETE request for the /del_user page.
-app.delete('/del_user', (req, res) => {
-    console.log("Got a DELETE request for /del_user");
-    res.send('Hello DELETE');
-})
-
-// This responds a GET request for the /list_user page.
-app.get('/list_user', (req, res) => {
-    console.log("Got a GET request for /list_user");
-    res.send('Page Listing');
-})
-
-// This responds a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', (req, res) => {
-    console.log("Got a GET request for /ab*cd");
-    res.send('Page Pattern Match');
-});
 
 var server = app.listen(8081, () => {
     var host = server.address().address
